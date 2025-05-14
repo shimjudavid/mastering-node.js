1eac706455ae2a7463187294155db3923bdd3bdf
Shimju David <shimjudavid@gmail.com>
2025-05-14 12:48:03 +0530

    added globals

---
+++ debug.js
@@ -0,0 +1,9 @@
+const obj = {
+  a: 'original value',
+  b: 6,
+};
+console.log(obj.a);
+obj.a = 'new value';
+console.log(obj.a);
+obj.b += 10;
+console.log(obj.b);
---
+++ global.js
@@ -0,0 +1,3 @@
+console.log(module);
+console.log(__filename); // /Users/username/projects/myapp
+console.log(__dirname); //
